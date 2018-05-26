90111a8634c9017db946586452a53e5b83626832
Nardberjean <Nardberjean@users.noreply.github.com>
2018-05-26 14:10:37 -0400

    Update time.py

--- time.py
+++ time.py
@@ -16,12 +16,13 @@ def run_command (cmd): #... missing argument
         os._exit(126) # errno == ENOENT ? 127 : 126
     print('ppid:_'+str(os.getpid())+'_')
     print('pid:__'+str(pid)+'_')
-    statusf = open('/proc/'+str(pid)+'/status', 'r') # This is not from time.c
+    cmdlinef = open('/proc/'+str(pid)+'/cmdline', 'r') # This is not from time.c
+    statusf = open('/proc/'+str(pid)+'/status', 'r')
     iof = open('/proc/'+str(pid)+'/io', 'r')
-    print(statusf.read())
-    print(iof.read())
+    print(cmdlinef.read())
     os.wait4(pid, 0)
     # print(statusf.read()) # ProcessLookupError: [Errno 3] No such process
     # print(io.read())
 
+run_command(["sleep", "1"])
 run_command(["whoami"])
